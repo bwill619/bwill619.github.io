96ada17b6c3792a5db07706269d494c0e077fc79
Brendon Williams <bjw0619@gmail.com>
2021-05-14 19:20:59 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+//index scripts
+function homeClick(){
+    location.href = 'index.html';
+}
+function aboutClick(){
+    location.href = 'about.html';
+}
+function projectsClick(){
+    location.href = 'projects.html';
+}
+function resumeClick(){
+    window.open('brendonresume.pdf');
+}
+
+function show(shown, hidden) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden).style.display='none';
+    return false;
+  }
+
+//about scripts
+
+//projects scripts
+
+
