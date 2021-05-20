cf8ec284f73ea022c6c6ca0b808468315b854cf9
Brendon Williams <bjw0619@gmail.com>
2021-05-20 01:01:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 //index scripts
-function homeClick(){
-    location.href = 'index.html';
+function homeClick() {
+  location.href = "index.html";
 }
-function aboutClick(){
-    location.href = 'about.html';
+function aboutClick() {
+  location.href = "about.html";
 }
-function projectsClick(){
-    location.href = 'projects.html';
+function projectsClick() {
+  location.href = "projects.html";
 }
-function resumeClick(){
-    window.open('brendonresume.pdf');
+function resumeClick() {
+  window.open("brendonresume.pdf");
 }
-
-function show(shown, hidden) {
-    document.getElementById(shown).style.display='block';
-    document.getElementById(hidden).style.display='none';
-    return false;
-  }
-
-//about scripts
-
-//projects scripts
-
-
